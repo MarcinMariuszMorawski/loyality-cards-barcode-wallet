3a84b47cdce3d65b46b045c8637e5328d9745276
Marcin <marcineq95@hotmail.com>
2019-06-01 12:22:04 +0200

    Initial commit

---
+++ src/main/java/pl/lodz/uni/math/loyalitycardsbarcodewalletapi/LoyalityCardsBarcodeWalletApiApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.uni.math.loyalitycardsbarcodewalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyalityCardsBarcodeWalletApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyalityCardsBarcodeWalletApiApplication.class, args);
+    }
+
+}
